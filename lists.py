8fc6855273300f40ee169b6178d3321c65ba289b
Gwen Murphy <gwenmurphy1997@gmail.com>
2022-11-07 14:51:29 +0000

    I've got no ideas yet.

---
+++ lists.py
@@ -0,0 +1,62 @@
+#/usr/bin/python3
+# *-*- coding utf-8 -*-
+
+import random
+import time
+
+printerlist = [
+    'RICOH IM 2500',
+    'RICOH IM 2702',
+    'RICOH IM 3000',
+    'RICOH IM 350',
+    'RICOH IM 3500',
+    'RICOH IM 350F',
+    'RICOH IM 4000',
+    'RICOH IM 430F',
+    'RICOH IM 5000A',
+    'RICOH IM 550F',
+    'RICOH IM 6000',
+    'RICOH IM 600F',
+    'RICOH IM 600SRF',
+    'RICOH IM 7000',
+    'RICOH IM 8000',
+    'RICOH IM 9000',
+    'RICOH IM C2000',
+    'RICOH IM C2000A',
+    'RICOH IM C2000LT',
+    'RICOH IM C2500A',
+    'RICOH IM C300',
+    'RICOH IM C3000',
+    'RICOH IM C3000A',
+    'RICOH IM C3000LT',
+    'RICOH IM C300F',
+    'RICOH IM C3500',
+    'RICOH IM C3500A',
+    'RICOH IM C400F',
+    'RICOH IM C400SRF',
+    'RICOH IM C4500',
+    'RICOH IM C4500A',
+    'RICOH IM C4500LT',
+    'RICOH IM C530F',
+    'RICOH IM C530FB',
+    'RICOH IM C5500',
+    'RICOH IM C5500A',
+    'RICOH IM C5500ALT',
+    'RICOH IM C6000',
+    'RICOH IM C6000LT',
+    'RICOH IM C6500'
+    'RICOH IM C8000',
+    'RICOH MP 3554',
+    'RICOH MP C6004'
+]
+
+print(f'Oh no, it\'s a {random.choice(printerlist)}!')
+
+"""
+Honestly, this file is just here while I think of an idea for a python project that utilises full stack too.
+That could take a while, though.
+
+Only thought of this thing off the top of my head because Bossman hates printers on a scale hitherto undreamt of.
+
+As to how full stack would be incorporated into it, I have less than no idea right now.
+"""
